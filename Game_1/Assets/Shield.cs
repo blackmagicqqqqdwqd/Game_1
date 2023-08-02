5b1bbe9b3779a3af02b3e507120005d010c78ba7
Mqwelli <141118776+Mqwelli@users.noreply.github.com>
2023-08-02 21:08:10 +1000

    Shield CS source

--- Game_1/Assets/Shield.cs
+++ Game_1/Assets/Shield.cs
@@ -13,7 +13,7 @@ public class Shield : MonoBehaviour
     void Update()
     {
         ChangeColor();
-        Debug.Log("прелээстно");
+        Debug.Log("Welcome");
     }
     void ChangeColor()
     {
