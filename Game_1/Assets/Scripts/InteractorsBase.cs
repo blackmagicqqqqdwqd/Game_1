fc9bb87e27785f1344e28872023806b47de23f27
blackmagicqqqqdwqd <danilom31@mail.ru>
2023-08-02 13:17:44 +0300

    6

--- Game_1/Assets/Scripts/InteractorsBase.cs
+++ Game_1/Assets/Scripts/InteractorsBase.cs
@@ -1,37 +1,8 @@
 using System;
 using System.Collections.Generic;
 
-<<<<<<< HEAD
+
 public abstract class InteractorsBase 
 {
-    Dictionary<Type ,Interactor> Interactors;
-    public abstract void Initialize();
-    public void CreatInteractor<T>() where T: Interactor,new() {
-        T interactor = new T();
-        Interactors[typeof(T)] = interactor;
-=======
-public class InteractorsBase 
-{
-    Dictionary<Type ,Interactor> interactors;
-    public void CreatInteractor<T>() where T: Interactor,new() {
-        T interactor = new T();
-        interactors[typeof(T)] = interactor;
-    }
-    public void Remove<T>() where T:Interactor
-    {
-        interactors.Remove(typeof(T));
-    }
-      public Interactor GetInteractor<T>() where T: Interactor
-    {
-        return interactors[typeof(T)];        
-    }
-    public void InitializeAllInteractor()
-    {
-        foreach (var interactor in interactors)
-        {
-           // interactor.
-        }
 
->>>>>>> main
-    }
 }
