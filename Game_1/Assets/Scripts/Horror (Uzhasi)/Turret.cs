761e40e81f9eac11430639184e630d5df87eb3a3
Mqwelli <141118776+Mqwelli@users.noreply.github.com>
2023-08-04 09:57:45 +1000

    turret anim v1

--- Game_1/Assets/Scripts/Horror (Uzhasi)/Turret.cs
+++ Game_1/Assets/Scripts/Horror (Uzhasi)/Turret.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     LineRenderer lr;
+    SpriteRenderer sr;
     Vector3 targetVector;
     public GameObject target;
     float initLength = 0.125f;
@@ -14,6 +15,7 @@ public class Turret : MonoBehaviour
     void Start()
     {
         lr = GetComponent<LineRenderer>();
+        sr = GetComponent<SpriteRenderer>();
         lr.SetWidth(0.25f, 0.25f);
         lr.SetColors(Color.magenta, Color.magenta);
         lr.SetPosition(0, new Vector3(transform.position.x, transform.position.y, transform.position.z));
@@ -21,17 +23,32 @@ public class Turret : MonoBehaviour
     }
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            StartCoroutine(ShootTarget(target));
-        }
+        if(Input.GetKeyDown(KeyCode.Space)) StartCoroutine(ShootPrepare());            
+        
     }
     IEnumerator ShootTarget(GameObject target)
-    {
+    {        
         yield return new WaitForSeconds(0.025f);
             lr.SetPosition(1, Vector3.Lerp(lr.GetPosition(0), target.transform.position, length + initLength/4));
         length += initLength/4;
-            if(lr.GetPosition(0) != lr.GetPosition(1)) StartCoroutine(ShootTarget(target));
+        if (lr.GetPosition(1) != target.transform.position) { Debug.Log("2"); StartCoroutine(ShootTarget(target)); }
+        else
+        {
+            yield return new WaitForSeconds(0.25f);
+            lr.SetPosition(1, lr.GetPosition(0));
+        }
         
+    }
+    IEnumerator ShootPrepare()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            sr.color = Color.red;
+            yield return new WaitForSeconds(0.2f);
+            sr.color = Color.white;
+            yield return new WaitForSeconds(0.2f);
+            Debug.Log("1");
+        }
+        StartCoroutine(ShootTarget(target));
     }
 }
