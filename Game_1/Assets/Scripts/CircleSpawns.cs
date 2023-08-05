45984eee13791b56350ebbafb25b98fcd2ca597b
Mqwelli <141118776+Mqwelli@users.noreply.github.com>
2023-08-05 20:46:39 +1000

    Захламление коммитов 2 - Нашествие

--- Game_1/Assets/Scripts/CircleSpawns.cs
+++ Game_1/Assets/Scripts/CircleSpawns.cs
@@ -5,15 +5,15 @@ using UnityEngine.U2D;
 
 public class CircleSpawns : MonoBehaviour
 {
-    float radius = 5f;
-    int spawnsAmount = 10;
-    float circleRotation = 75.8f;
-    Vector3 coordinates = new Vector3(-5, 1, 0);
+    //float radius = 5f;
+    //int spawnsAmount = 10;
+    //float circleRotation = 75.8f;
+    //Vector3 coordinates = new Vector3(-5, 1, 0);
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)) { foreach(Vector3 v in CreateSpawns(radius, spawnsAmount, circleRotation, coordinates)) {Debug.Log(v); };}
+        if(Input.GetKeyDown(KeyCode.Space)) { foreach(Vector3 vect in CircleSpawn(5f, 10, 75, new Vector3(-5,1,0))) {Debug.Log(vect); };}
     }
-    public List<Vector3> CreateSpawns(float rad, int amount, float rot, Vector3 coord)
+    public List<Vector3> CircleSpawn(float rad, int amount, float rot, Vector3 coord)
     {
         if (rad > 0 && amount > 0)
         {
@@ -21,7 +21,7 @@ public class CircleSpawns : MonoBehaviour
             for (int i = 1; i <= amount; ++i)
             {
                 GameObject go = new GameObject("Spawned"+i);
-                go.transform.position = new Vector3(coord.x+radius * Mathf.Cos((360 / spawnsAmount) * i * Mathf.Deg2Rad + circleRotation * Mathf.Deg2Rad), coord.y+radius * Mathf.Sin((360 / spawnsAmount) * i * Mathf.Deg2Rad + circleRotation * Mathf.Deg2Rad), 0);
+                go.transform.position = new Vector3(coord.x+rad * Mathf.Cos((360 / amount) * i * Mathf.Deg2Rad + rot * Mathf.Deg2Rad), coord.y+rad * Mathf.Sin((360 / amount) * i * Mathf.Deg2Rad + rot * Mathf.Deg2Rad), 0);
                 go.AddComponent<SpriteRenderer>();
                 go.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/ProtectedCircle");
                 SpawnedObjects.Add(go.transform.position);
