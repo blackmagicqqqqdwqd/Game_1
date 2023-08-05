21e7baf24b4e2dcf276c797de23a160ca3535ee6
Mqwelli <141118776+Mqwelli@users.noreply.github.com>
2023-08-05 20:38:39 +1000

    Захламление коммитов

--- Game_1/Assets/Scripts/CircleSpawns.cs
+++ Game_1/Assets/Scripts/CircleSpawns.cs
@@ -8,23 +8,26 @@ public class CircleSpawns : MonoBehaviour
     float radius = 5f;
     int spawnsAmount = 10;
     float circleRotation = 75.8f;
-    GameObject circumflex;
-
+    Vector3 coordinates = new Vector3(-5, 1, 0);
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)) { CreateSpawns(radius,spawnsAmount,circleRotation); }
+        if(Input.GetKeyDown(KeyCode.Space)) { foreach(Vector3 v in CreateSpawns(radius, spawnsAmount, circleRotation, coordinates)) {Debug.Log(v); };}
     }
-    public void CreateSpawns(float rad, int amount, float rot)
+    public List<Vector3> CreateSpawns(float rad, int amount, float rot, Vector3 coord)
     {
         if (rad > 0 && amount > 0)
         {
+            List<Vector3> SpawnedObjects = new List<Vector3>();
             for (int i = 1; i <= amount; ++i)
             {
-                GameObject go = new GameObject();
-                go.transform.position = new Vector3(radius * Mathf.Cos((360 / spawnsAmount) * i * Mathf.Deg2Rad + circleRotation * Mathf.Deg2Rad), radius * Mathf.Sin((360 / spawnsAmount) * i * Mathf.Deg2Rad + circleRotation * Mathf.Deg2Rad), 0);
+                GameObject go = new GameObject("Spawned"+i);
+                go.transform.position = new Vector3(coord.x+radius * Mathf.Cos((360 / spawnsAmount) * i * Mathf.Deg2Rad + circleRotation * Mathf.Deg2Rad), coord.y+radius * Mathf.Sin((360 / spawnsAmount) * i * Mathf.Deg2Rad + circleRotation * Mathf.Deg2Rad), 0);
                 go.AddComponent<SpriteRenderer>();
                 go.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/ProtectedCircle");
+                SpawnedObjects.Add(go.transform.position);
             }
+            return SpawnedObjects;
         }
+        else return null;
     }
 }
